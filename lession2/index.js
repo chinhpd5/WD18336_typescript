7a42eb9d0f93ffc56b545a1e024d56940a438558
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-06 16:51:26 +0700

    a

---
+++ lession2/index.js
@@ -0,0 +1,11 @@
+var myName = 'Chinhpd5';
+var myNumber = 18;
+var isCheack = true;
+function sum(a, b) {
+    return a + b;
+}
+function showName(name) {
+    return "xin ch\u00E0o ".concat(name);
+}
+showName(myName);
+// console.log(myName);
---
+++ lession2/index.ts
@@ -0,0 +1,18 @@
+let myName: string = 'Chinhpd5';
+let myNumber: number = 18;
+let isCheack: boolean = true;
+
+
+function sum(a: number, b: number){
+    return a+b;
+}
+
+function showName(name:string): string{
+    return `xin chào ${name}`
+}
+
+
+showName(myName);
+
+
+// console.log(myName);
