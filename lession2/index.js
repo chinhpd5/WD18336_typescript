c7bd258a1c11ff099283a0adb4e252fb1ccef38f
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-06 17:35:46 +0700

    a

--- lession2/index.js
+++ lession2/index.js
@@ -7,5 +7,36 @@ function sum(a, b) {
 function showName(name) {
     return "xin ch\u00E0o ".concat(name);
 }
-showName(myName);
-// console.log(myName);
+var array = ["Nam", "Bắc", "Hồng"];
+var array2 = ["a", 1];
+//tsc index.ts --watch
+var obj = {
+    name: "chinhpd5",
+    age: 18,
+    gt: false
+};
+obj.gt = true;
+// console.log(obj);
+var people = [
+    {
+        name: "chinhpd5",
+        age: 18,
+        gt: false
+    },
+    {
+        name: "cuong",
+        age: 28,
+        gt: true
+    },
+    {
+        name: "long",
+        age: 8,
+        gt: false
+    }
+];
+function showData(data) {
+    return data.map(function (item) {
+        return "\n            <div>\n                <h3>".concat(item.name, "</h3>\n                <h4>").concat(item.age, "</h4>\n                <p>").concat((item.gt ? 'Nam' : 'Nữ'), "</p>\n            </div>\n        ");
+    }).join('');
+}
+console.log(showData(people));
--- lession2/index.ts
+++ lession2/index.ts
@@ -11,8 +11,63 @@ function showName(name:string): string{
     return `xin chào ${name}`
 }
 
+let array: string[] | number[] = ["Nam", "Bắc", "Hồng"]
 
-showName(myName);
+let array2: (string|number)[] = ["a",1]
+
+//tsc index.ts --watch
+let obj:{
+    name: string,
+    age: number
+    gt: boolean
+} = {
+    name: "chinhpd5",
+    age: 18,
+    gt: false
+}
+
+obj.gt = true
+
+// console.log(obj);
 
 
-// console.log(myName);
+let people: { name: string,age: number, gt: boolean}[] =[
+    {
+        name: "chinhpd5",
+        age: 18,
+        gt: false
+    },
+    {
+        name: "cuong",
+        age: 28,
+        gt: true
+    },
+    {
+        name: "long",
+        age: 8,
+        gt: false
+    }
+
+]
+
+function showData(data: { name: string,age: number, gt: boolean}[])
+:string{
+    return data.map(item=>{
+        return `
+            <div>
+                <h3>${item.name}</h3>
+                <h4>${item.age}</h4>
+                <p>${(item.gt?'Nam':'Nữ')}</p>
+            </div>
+        `
+    }).join('');
+    
+}
+
+console.log(showData(people));
+
+
+
+
+
+
