de30dbbc0eb20d99dfa7ee1a23c1bc6c5c5835df
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-08 17:54:15 +0700

    lession3

--- lession2/index.js
+++ lession2/index.js
@@ -8,7 +8,7 @@ function showName(name) {
     return "xin ch\u00E0o ".concat(name);
 }
 var array = ["Nam", "Bắc", "Hồng"];
-var array2 = ["a", 1];
+var array2 = ["a", 1, "b"];
 //tsc index.ts --watch
 var obj = {
     name: "chinhpd5",
@@ -16,18 +16,29 @@ var obj = {
     gt: false
 };
 obj.gt = true;
+var arr2 = [
+    {
+        id: 1,
+        name: "chinhpd5",
+        age: 18,
+        gt: false
+    },
+];
 var people = [
     {
+        id: 1,
         name: "chinhpd5",
         age: 18,
         gt: false
     },
     {
+        id: 2,
         name: "cuong",
         age: 28,
         gt: true
     },
     {
+        id: 3,
         name: "long",
         age: 8,
         gt: false
@@ -35,16 +46,43 @@ var people = [
 ];
 function showData(data) {
     return data.map(function (item) {
-        return "\n            <div>\n                <h3>".concat(item.name, "</h3>\n                <h4>").concat(item.age, "</h4>\n                <p>").concat((item.gt ? 'Nam' : 'Nữ'), "</p>\n            </div>\n        ");
+        return "\n            <div>\n                <h3>".concat(item.id, "</h3>\n                <h3>").concat(item.name, "</h3>\n                <h4>").concat(item.age, "</h4>\n                <p>").concat((item.gt ? 'Nam' : 'Nữ'), "</p>\n            </div>\n        ");
     }).join('');
 }
 function addData(data) {
     people.push(data);
 }
-var peo = {
+var person = {
+    id: 4,
     name: "long",
     age: 8,
     gt: false
 };
-addData(peo);
+// addData(person);
+var editData = function (id, data) {
+    //code
+    //b1: lấy vị trí của id
+    var index = people.findIndex(function (item) { return item.id == id; });
+    //b2 thay thế data vào vị trí của people
+    people[index] = data;
+};
+// editData(3,person);
+//deleteData
+var deleteData = function (id) {
+    //sử dụng hàm splice xóa 1 phần tử trong mảng
+    var index = people.findIndex(function (item) { return item.id == id; });
+    people.splice(index, 1);
+};
 console.log(showData(people));
+//searchData theo name 
+var searchData = function (searchString) {
+    // filter ,includes, find, 
+    return people.filter(function (item) {
+        return item.name.includes(searchString);
+    });
+};
+//tsc index.ts --watch
+var tuple = [10, 'chinhpd5'];
+// tuple.push('chinh');
+var union = "Thất bại";
+// union = "Thành công";
--- lession2/index.ts
+++ lession2/index.ts
@@ -7,13 +7,13 @@ function sum(a: number, b: number){
     return a+b;
 }
 
-function showName(name:string): string{
+function showName(name:string): string{ //void
     return `xin chào ${name}`
 }
 
-let array: string[] | number[] = ["Nam", "Bắc", "Hồng"]
+let array: string[] = ["Nam", "Bắc", "Hồng"]
 
-let array2: (string|number)[] = ["a",1]
+let array2: (string|number)[] = ["a",1,"b"]
 
 //tsc index.ts --watch
 let obj:{
@@ -31,23 +31,47 @@ obj.gt = true
 // console.log(obj);
 
 type myType ={
+    id: number,
     name: string,
     age: number, 
     gt: boolean
 }
 
+interface ImyType {
+    id: number,
+    name: string,
+    age: number, 
+    gt: boolean
+}
+
+interface ImyType2 extends ImyType {
+    description?: string
+}
+
+let arr2: ImyType2[] = [
+    {
+        id:1,
+        name: "chinhpd5",
+        age: 18,
+        gt: false
+    },
+]
+
 let people: myType[] =[
     {
+        id:1,
         name: "chinhpd5",
         age: 18,
         gt: false
     },
     {
+        id:2,
         name: "cuong",
         age: 28,
         gt: true
     },
     {
+        id:3,
         name: "long",
         age: 8,
         gt: false
@@ -59,6 +83,7 @@ function showData(data: myType[]):string{
     return data.map(item=>{
         return `
             <div>
+                <h3>${item.id}</h3>
                 <h3>${item.name}</h3>
                 <h4>${item.age}</h4>
                 <p>${(item.gt?'Nam':'Nữ')}</p>
@@ -72,16 +97,52 @@ function addData(data: myType):void{
     people.push(data);
 }
 
-let peo: myType={
+let person: myType={
+    id:4,
     name: "long",
     age: 8,
     gt: false
 }
 
-addData(peo);
+// addData(person);
 
 
+const editData = (id:number, data: myType ):void =>{
+    //code
+    //b1: lấy vị trí của id
+    const index:number = people.findIndex( item => item.id == id);
+    
+    //b2 thay thế data vào vị trí của people
+    people[index] = data;
+}
+// editData(3,person);
+
+
+//deleteData
+const deleteData = (id: number):void=>{
+    //sử dụng hàm splice xóa 1 phần tử trong mảng
+    const index:number = people.findIndex( item => item.id == id);
+
+    people.splice(index,1);
+}
 console.log(showData(people));
+
+//searchData theo name 
+const searchData =(searchString: string):any =>{
+    // filter ,includes, find, 
+    return people.filter(item=>{
+        return item.name.includes(searchString);
+    });
+}
+//tsc index.ts --watch
+
+let tuple: [number,string] = [10,'chinhpd5']
+
+// tuple.push('chinh');
+
+let union: number | string = "Thất bại";
+
+// union = "Thành công";
 
 
 
