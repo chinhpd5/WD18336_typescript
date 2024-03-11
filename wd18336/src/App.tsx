040fdb86303cb51af531ddb5097dbd0c2c4af8ed
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-11 18:08:41 +0700

    b4.4

--- wd18336/src/App.tsx
+++ wd18336/src/App.tsx
@@ -34,6 +34,39 @@ function ShowProduct(props: ProductType):JSX.Element{
 }
 
 function App() {
+
+  const arrProduct: ProductType[]=[
+    {
+      id: 1,
+      name: "produc 1",
+      price: "100.000",
+      sale: true,
+      description: "mô tả 1",
+    },
+    {
+      id: 2,
+      name: "produc 2",
+      price: "200.000",
+      sale: true,
+      description: "mô tả 2",
+    },
+    {
+      id: 3,
+      name: "produc 3",
+      price: "300.000",
+      sale: true,
+      description: "mô tả 3",
+    },
+    {
+      id: 4,
+      name: "produc 4",
+      price: "400.000",
+      sale: true,
+      description: "mô tả 4",
+    }
+  ]
+  //BTVN render arrProdcut 
+
   return (
     <>
       <ShowProduct id={1} name="Product 2" 
