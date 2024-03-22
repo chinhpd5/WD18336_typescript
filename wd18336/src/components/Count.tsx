1c32852b2665b617a554e5d907ed9aa9d2ac6861
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-22 17:30:51 +0700

    b9.2

--- wd18336/src/components/Count.tsx
+++ wd18336/src/components/Count.tsx
@@ -1,32 +1,69 @@
 import { useReducer, useState } from "react";
 
 function reducer(state:any,action:any){
+    console.log("action: ",action);
+    console.log("state trước: ",state);
+    
     switch(action){
         case "ADD":
-            return state + 1;
+            state = state + 1
+            break;
+            //return state + 1
         case "DELETE":
-            return state -1;
+            state = state -1;
+            break;
+            //return state - 1
         case "RESET":
-            return 0
+            state = 0
+            break;
+            //return 0
         default: 
             return state;
     }
+    console.log("state sau:",state);
+    return state;
+}
+function reducer2(state:any, action:any){
+    console.log(action);
+    switch(action.type){
+        case "GAN":
+            // state = action.payload;
+            // break;
+            return action.payload;
+        default:
+            state;
+    }
+    // return state;
 }
 
 function Count(){
     // const [count,setCount] = useState(0);
     const [count,dispatch] = useReducer(reducer,0)
+    const [count2,dispatch2] = useReducer(reducer2,0)
 
     return (
        <>
-            <h2>{count}</h2>
+            <div>
+                <h2>{count}</h2>
 
-            <button className="btn btn-success mx-2" 
-                onClick={()=>{dispatch("ADD")}}>Tăng</button>
-            <button className="btn btn-danger mx-2" 
-                onClick={()=> {dispatch("DELETE")}}>Giảm</button>
-            <button className="btn btn-warning mx-2 " 
-                onClick={()=>{dispatch("RESET")}}>Reset</button>
+                <button className="btn btn-success mx-2" 
+                    onClick={()=>{dispatch("ADD")}}>Tăng</button>
+                <button className="btn btn-danger mx-2" 
+                    onClick={()=> {dispatch("DELETE")}}>Giảm</button>
+                <button className="btn btn-warning mx-2 " 
+                    onClick={()=>{dispatch("RESET")}}>Reset</button>
+            </div>
+
+            <div>
+                <h2>{count2}</h2>
+                <button className="btn btn-success mx-2" 
+                    onClick={
+                        ()=>{dispatch2({type: "GAN",payload: 10})}
+                    }>
+                        Gán gt
+                </button>
+
+            </div>
        </>
     )
 }
