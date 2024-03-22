e6b9c990675050b8aeb0755d4026e644c579d242
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-22 18:02:40 +0700

    b9.3

--- wd18336/src/components/Count.tsx
+++ wd18336/src/components/Count.tsx
@@ -1,4 +1,6 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+import IProduct from "../interfaces/IProduct";
+import ProductList from "./Product/ProductList";
 
 function reducer(state:any,action:any){
     console.log("action: ",action);
@@ -35,14 +37,61 @@ function reducer2(state:any, action:any){
     }
     // return state;
 }
+const initProduct={
+    listProduct: [] as IProduct[],
+    isLoading: false
+}
+
+function reducerProduct(state:any, action: any){
+    
+    switch(action.type){
+        case "LOADING":
+            return {
+                ...state,
+                isLoading: true
+            }
+        case "DONE":
+            return {
+                ...state,
+                isLoading: false,
+                listProduct: action.payload
+            }
+        default:
+            return state;
+    }
+}
 
 function Count(){
     // const [count,setCount] = useState(0);
     const [count,dispatch] = useReducer(reducer,0)
     const [count2,dispatch2] = useReducer(reducer2,0)
+    const [product,dispatchProduct] = useReducer(reducerProduct,initProduct);
+
+    function handleGetData(){
+        
+        // b1 tạo loading
+        dispatchProduct({type: "LOADING"});
+
+        // b2 gọi dữ liệu
+        setTimeout(()=>{
+                // product
+            fetch(`http://localhost:3000/product`)
+                .then(data=>{
+                return data.json();
+                })
+                .then(data=>{
+                    dispatchProduct({type: "DONE", payload: data});
+                })
+        },3000)
+       
+    }
 
     return (
-       <>
+        <>
+            <div>
+                <button onClick={handleGetData}>Get Data</button>
+                {product.isLoading ? <h2>Loading ....</h2> : <ProductList listProduct={product.listProduct} onDelete={()=>{}} /> }
+            </div>
             <div>
                 <h2>{count}</h2>
 
