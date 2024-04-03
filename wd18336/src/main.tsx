b60ee1bec089ddc70a36b9d8a79ba2cc4b4518f9
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-04-03 16:27:38 +0700

    giao diện

--- wd18336/src/main.tsx
+++ wd18336/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CountProvider } from './context/CountProvider.tsx';
 import { ProductProvider } from './context/ProductProvider.tsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ProductProvider>
--- wd18336/src/pages/layout/WebsiteLayout.tsx
+++ wd18336/src/pages/layout/WebsiteLayout.tsx
@@ -2,7 +2,6 @@ import { Link, Outlet } from "react-router-dom";
 
 
 function WebsiteLayout(){
-
     return(
         <>
             <header>
@@ -10,24 +9,31 @@ function WebsiteLayout(){
             <nav className="navbar navbar-expand-lg bg-success navbar-dark">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/" >Trang chủ</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/product">Sản phẩm</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/contact">Liên hệ</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/cart">Giỏ hàng</Link>
-                        </li>
-                    </ul>
+                    <div className="navbar-collapse d-flex justify-content-between" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/product">Sản phẩm</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/contact">Liên hệ</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/cart">Giỏ hàng</Link>
+                            </li>
+                        </ul>
+
+                        <ul className="navbar-nav me-3">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Đăng kí</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Đăng nhập</Link>
+                            </li>
+                        </ul>
                     </div>
+
                 </div>
-                </nav>
+            </nav>
 
             </header>
             <main>
